13c2911200d30fd855f159e0da184f0dfa039449
Claire V. Hammond <me@cvhammond.com>
2024-05-30 00:08:24 -0700

    add scope to example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,4 @@
-use egui::*;
+use egui::{DragValue, Separator};
 use egui_drag_value_expr::DragValueExpr;
 
 fn main() {
@@ -20,9 +20,18 @@ struct App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.add(&mut DragValueExpr::new(&mut self.value, None));
-            });
+            ui.add(DragValueExpr::new(&mut self.value, None));
+            ui.add(Separator::default());
+            ui.add(DragValueExpr::from_drag_value(
+                    DragValue::new(&mut self.value)
+                    .fixed_decimals(2),
+                    None,
+            ));
+            ui.add(Separator::default());
+            let mut scope = rhai::Scope::new();
+            scope.push("ten", 10 as i64);
+            scope.push("self", self.value);
+            ui.add(DragValueExpr::new(&mut self.value, Some(&scope)));
         });
     }
 }
