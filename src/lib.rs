cc175b2cce9dfc37d7611e81b164cabd7c5fb308
Claire V. Hammond <me@cvhammond.com>
2024-05-30 00:08:09 -0700

    add from_drag_value to pass settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,42 @@
 use egui::*;
-use rhai::{Engine, Scope};
+use rhai::{Dynamic, Engine, Scope};
 
 pub struct DragValueExpr<'a> {
-    value: &'a mut f64,
     scope: Option<&'a Scope<'a>>,
+    drag_value: DragValue<'a>,
 }
 
 impl<'a> DragValueExpr<'a> {
     pub fn new(value: &'a mut f64, scope: Option<&'a Scope>) -> Self {
-        Self { value, scope }
+        Self {
+            scope,
+            drag_value: DragValue::new(value),
+        }
+    }
+
+    pub fn from_drag_value(drag_value: DragValue<'a>, scope: Option<&'a Scope>) -> Self {
+        Self { scope, drag_value }
     }
 }
 
-impl<'a> Widget for &mut DragValueExpr<'a> {
+impl<'a> Widget for DragValueExpr<'a> {
     fn ui(self, ui: &mut Ui) -> Response {
-        ui.add(DragValue::new(self.value).custom_parser(|s| {
+        ui.add(self.drag_value.custom_parser(|s| {
             let engine = Engine::new();
             let mut scope = match self.scope {
                 Some(scope) => scope.clone(),
                 None => Scope::new(),
             };
-            let result = engine.eval_expression_with_scope::<f64>(&mut scope, s);
+            let result = engine.eval_expression_with_scope::<Dynamic>(&mut scope, s);
             match result {
                 Ok(result) => {
-                    dbg!(result);
-                    Some(result)
+                    if result.is::<f64>() {
+                        Some(result.cast::<f64>())
+                    } else if result.is::<i64>() {
+                        Some(result.cast::<i64>() as f64)
+                    } else {
+                        None
+                    }
                 }
                 Err(err) => {
                     eprintln!("Error: {}", err);
@@ -32,20 +44,5 @@ impl<'a> Widget for &mut DragValueExpr<'a> {
                 }
             }
         }))
-    }
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
